e3b3f98fc2c54220e0eccb8bc5cc4fbf6a58b353
Miguel Angel <miguelangelhh17@gmail.com>
2024-01-20 11:23:47 -0500

    config chatbot with astra db

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="chatbot-text-tertiary flex justify-end text-sm mt-6">
+      <div className="ml-auto flex flex-row items-center">
+        <a href="https://www.linkedin.com/in/miguel1man/">MH 2024</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
